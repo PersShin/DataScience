edefaf016866f072ca5cc1390532e46789061432
PersShin <shinryan9@naver.com>
2021-11-19 22:10:57 +0900

    새로운 공모전을 하다가 써본 코드

---
+++ 한 파일에 있는 csv 합치기.py
@@ -0,0 +1,12 @@
+#read_file()
+input_file = r'C:\scripts\지하철역내대기환경\하이' # csv파일들이 있는 디렉토리 위치
+allFile_list = glob.glob(os.path.join(input_file, '지하철초미세먼지*')) # glob함수로 sales_로 시작하는 파일들을 모은다
+print(allFile_list)
+allData = [] # 읽어 들인 csv파일 내용을 저장할 빈 리스트를 하나 만든다
+for file in allFile_list:
+    df = pd.read_csv(file)
+    tmp=df.loc[df['위치']=='서울']
+    allData.append(tmp)
+dataCombine = pd.concat(allData, axis=0, ignore_index=True) # concat함수를 이용해서 리스트의 내용을 병합
+# axis=0은 수직으로 병합함. axis=1은 수평. ignore_index=True는 인데스 값이 기존 순서를 무시하고 순서대로 정렬되도록 한다.
+dataCombine.to_csv('./서울지하철역내대기환경.csv', index=False, header=True,encoding="utf-8-sig") # to_csv함수로 저장한다. 인데스를 빼려면 False로 설정
