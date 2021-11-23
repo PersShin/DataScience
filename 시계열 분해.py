c1a81433fe6d1c6b9e5b1450bd70cb5aafd801f1
PersShin <shinryan9@naver.com>
2021-11-23 21:47:39 +0900

    데이터가 없는 경우 추가

--- 시계열 분해.py
+++ 시계열 분해.py
@@ -1,6 +1,11 @@
+#만약 station안에 원하는 값이 없다면 try except를 사용해서 오류를 파해쳐보자!
+import matplotlib.pyplot as plt
 for i in station:
-    tmp=df[df['Wind']]
-    tmp.reset_index(drop=True,inplace=True)
-    res = seasonal_decompose(tmp,model='additive', period=306)
-    ax=res.plot()
-    plt.show()
+    try:
+        tmp=df[df['Wind']==i]
+        tmp.reset_index(drop=True,inplace=True)
+        res = seasonal_decompose(tmp,model='additive', period=306)
+        ax=res.plot()
+        plt.show()
+    except:
+        print(end='')
